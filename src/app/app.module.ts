d35ab4f83dd91ac8b62b05cf44f5d0e0b8b3ceef
Bruno Jullien <bruno.jullien@gmail.com>
2023-02-11 19:40:04 -0800

    31. Implementing Hints and Validation Errors (on Forms)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material.module";
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { TrainingComponent } from './training/training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {LoginComponent} from './auth/login/login.component';
+import {TrainingComponent} from './training/training.component';
+import {CurrentTrainingComponent} from './training/current-training/current-training.component';
+import {NewTrainingComponent} from './training/new-training/new-training.component';
+import {PastTrainingsComponent} from './training/past-trainings/past-trainings.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 import {AppRoutingModule} from "./app-routing.module";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -29,7 +31,9 @@ import {AppRoutingModule} from "./app-routing.module";
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
